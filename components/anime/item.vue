c57ea134fe0d06af2059848deaaa88fa627913d9
Reda Cherfaoui <redacherfaoui@gmail.com>
2024-11-24 15:30:23 +0100

    alpha

--- components/anime/item.vue
+++ components/anime/item.vue
@@ -22,7 +22,7 @@
         </div>
         <NuxtLink
           :to="`/anime/${content.id}`"
-          class="px-4 py-2 bg-primary-500 text-white rounded-md hover:bg-primary-600 transition-colors"
+          class="px-4 py-6 bg-primary-500 text-white rounded-md hover:bg-primary-600 transition-colors"
         >
           Details
         </NuxtLink>
