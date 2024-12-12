8fa38d987dad58ea8f464a6d587f01944347d815
Reda Cherfaoui <redacherfaoui@gmail.com>
2024-12-12 14:54:07 +0100

    test windsurf

---
+++ stores/user.ts
@@ -0,0 +1,45 @@
+import { defineStore } from 'pinia'
+
+interface UserState {
+  isAuthenticated: boolean
+  user: {
+    id: string | null
+    username: string | null
+    email: string | null
+  } | null
+}
+
+export const useUserStore = defineStore('user', {
+  state: (): UserState => ({
+    isAuthenticated: false,
+    user: null
+  }),
+
+  getters: {
+    // Get user information
+    getUserInfo: (state) => state.user,
+    // Check if user is authenticated
+    getIsAuthenticated: (state) => state.isAuthenticated
+  },
+
+  actions: {
+    // Login action
+    login(userData: { username: string; email: string; id: string }) {
+      this.isAuthenticated = true
+      this.user = userData
+    },
+
+    // Logout action
+    logout() {
+      this.isAuthenticated = false
+      this.user = null
+    },
+
+    // Update user information
+    updateUserInfo(userData: Partial<UserState['user']>) {
+      if (this.user) {
+        this.user = { ...this.user, ...userData }
+      }
+    }
+  }
+})
