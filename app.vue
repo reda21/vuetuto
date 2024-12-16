9c67c7bb34e9d1adb21b08911b7571d9e7c054e8
Reda Cherfaoui <redacherfaoui@gmail.com>
2024-12-16 15:02:05 +0100

    Refactor app.vue template, add socket.io-client dependency, and implement socket connection in index.vue. Cleaned up unused code and improved formatting for better readability.

--- app.vue
+++ app.vue
@@ -1,18 +1,19 @@
 <template>
   <NuxtLayout>
-    <NuxtLoadingIndicator/>
+    <NuxtLoadingIndicator />
     <NuxtRouteAnnouncer />
     <NuxtPage />
   </NuxtLayout>
 </template>
 
 <script setup lauguage="ts">
-//[nuxt] Your project has pages but the <NuxtPage /> component has not been used. You might be using the <RouterView /> component instead, which will not work correctly in Nuxt. You can set pages: false in nuxt.config if you do not wish to use the Nuxt vue-router integration.
 
-/*
-<div
-    class="min-h-screen bg-white dark:bg-gray-900 transition-colors duration-300"
-  >
-  </div>
-*/
+
+
+
+onMounted(() => {
+    
+
+
+});
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,20 @@
 <template>
-  <div class="dark:bg-gray-800 flex flex-col items-center justify-center min-h-screen gap-4">
+  <div
+    class="dark:bg-gray-800 flex flex-col items-center justify-center min-h-screen gap-4"
+  >
     <h1 class="text-2xl font-bold">Compteur Nuxt</h1>
     <div class="flex items-center gap-4">
-      <button @click="store.decrement"
-        class="px-4 py-2 text-white bg-red-500 rounded-md hover:bg-red-600 transition-colors">
+      <button
+        @click="store.decrement"
+        class="px-4 py-2 text-white bg-red-500 rounded-md hover:bg-red-600 transition-colors"
+      >
         -
       </button>
       <span class="text-xl font-semibold">{{ store.count }}</span>
-      <button @click="store.increment"
-        class="px-4 py-2 text-white bg-green-500 rounded-md hover:bg-green-600 transition-colors">
+      <button
+        @click="store.increment"
+        class="px-4 py-2 text-white bg-green-500 rounded-md hover:bg-green-600 transition-colors"
+      >
         +
       </button>
     </div>
@@ -16,7 +22,18 @@
 </template>
 
 <script setup lang="ts">
+import { io } from "socket.io-client";
 const store = useCounterStore();
 
+const socket = io("http://localhost:3000");
 
+socket.on("connect", () => {
+  console.info("is connected");
+  // Obtenir l'heure actuelle et la formater en HH:MM:SS au format français
+  const now = new Date();
+  const formattedTime = now.toLocaleTimeString("fr-FR", { hour12: false });
+
+  // Envoyer l'heure formatée
+  socket.emit("test", formattedTime);
+});
 </script>
