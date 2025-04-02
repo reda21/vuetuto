fd0b26519af376780ea3df3726ab73c5350bc827
reda cherfaoui <redacherfaoui@gmail.com>
2025-04-02 23:51:14 +0100

    add exeples styles forums pages

--- components/cardTuto.vue
+++ components/cardTuto.vue
@@ -1,14 +1,17 @@
 <template>
-  <div class="bg-light-second dark:bg-dark-second rounded-xl p-6 text-center shadow-xl mt-16 relative pt-22">
+  <div
+    class="bg-light-second dark:bg-dark-second relative mt-16 rounded-xl p-6 pt-22 text-center shadow-xl"
+  >
     <div
       class="absolute inset-x-0 -top-16 left-0 mx-auto flex h-32 w-32 transform items-center justify-center rounded-full shadow-lg"
-      :class="selectedColor">
+      :class="selectedColor"
+    >
       <component :is="selectedIcon" class="h-24 w-24 text-white" />
     </div>
     <h3 class="text-light-font dark:text-dark-font mb-3 text-xl font-medium">
       TREE AND SHRUB PRUNING
     </h3>
-    <p class="px-4 text-light-font dark:text-dark-font">
+    <p class="text-light-font dark:text-dark-font px-4">
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo iure inventore amet modi
       accusantium vero perspiciatis, incidunt dicta sed aspernatur!
     </p>
