ed927923efa9d666f434ec359f13306fb4fa3440
reda cherfaoui <redacherfaoui@gmail.com>
2025-04-02 20:23:55 +0100

    refactor(components): extract card component and update theme styles
    - Extract card logic into a reusable `cardTuto.vue` component
    - Update theme.css to fix semicolons and add `--color-orange`
    - Reorganize main.css imports and remove unused styles

---
+++ components/cardTuto.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="bg-light-second dark:bg-dark-second rounded-xl p-6 text-center shadow-xl mt-16 relative pt-22">
+    <div
+      class="absolute inset-x-0 -top-16 left-0 mx-auto flex h-32 w-32 transform items-center justify-center rounded-full shadow-lg"
+      :class="selectedColor">
+      <component :is="selectedIcon" class="h-24 w-24 text-white" />
+    </div>
+    <h3 class="text-light-font dark:text-dark-font mb-3 text-xl font-medium">
+      TREE AND SHRUB PRUNING
+    </h3>
+    <p class="px-4 text-light-font dark:text-dark-font">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo iure inventore amet modi
+      accusantium vero perspiciatis, incidunt dicta sed aspernatur!
+    </p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import SvgPhp from '@/components/svg/php.vue';
+import SvgJs from '@/components/svg/js.vue';
+import SvgCss3 from '@/components/svg/css3.vue';
+
+// @ts-ignore
+const props = defineProps<{
+  categorie: 'php' | 'js' | 'css';
+}>();
+
+const icons = {
+  php: SvgPhp,
+  js: SvgJs,
+  css: SvgCss3,
+};
+
+const colors = {
+  php: 'bg-[#777BB3] shadow-[#777BB3]', // Couleur officielle PHP
+  js: 'bg-[#F7DF1E] shadow-[#F7DF1E]', // Couleur officielle JavaScript
+  css: 'bg-[#264DE4] shadow-[#264DE4]', // Couleur officielle CSS3
+};
+
+const selectedIcon = computed(() => icons[props.categorie as keyof typeof icons]);
+const selectedColor = computed(() => colors[props.categorie as keyof typeof colors]);
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,49 +1,8 @@
 <template>
-  <div>
-    <div class="p-4 pt-22">
-      <div class="grid gap-14 md:grid-cols-3 md:gap-5">
-        <div class="bg-light-second dark:bg-dark-second rounded-xl p-6 text-center shadow-xl">
-          <div
-            class="mx-auto flex h-32 w-32 -translate-y-22 transform items-center justify-center rounded-full bg-[#687bae] shadow-lg shadow-[#687bae]"
-          >
-            <SvgPhp class="h-24 w-24 text-white" />
-          </div>
-          <h3 class="text-light-font dark:text-dark-font mb-3 text-xl font-medium lg:px-14">
-            TREE AND SHRUB PRUNING
-          </h3>
-          <p class="px-4 text-gray-500">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo iure inventore amet modi
-            accusantium vero perspiciatis, incidunt dicta sed aspernatur!
-          </p>
-        </div>
-        <div class="bg-light-02 rounded-xl p-6 text-center shadow-xl dark:bg-[#2e3267]">
-          <div
-            class="shadows mx-auto flex h-32 w-32 -translate-y-22 transform items-center justify-center rounded-full bg-rose-500 shadow-rose-500/40"
-          >
-            <SvgJs class="h-24 w-24 text-white" />
-          </div>
-          <h1 class="text-darken mb-3 text-xl font-medium lg:px-14">IRRIGATION & DRAINAGE</h1>
-          <p class="px-4 text-gray-500">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo iure inventore amet modi
-            accusantium vero perspiciatis, incidunt dicta sed aspernatur!
-          </p>
-        </div>
-        <div class="bg-light-02 rounded-xl p-6 text-center shadow-xl dark:bg-[#2e3267]">
-          <div
-            class="mx-auto flex h-32 w-32 -translate-y-22 transform items-center justify-center rounded-full bg-sky-500 shadow-lg shadow-sky-500/40"
-          >
-            <SvgCss3 class="h-24 w-24 text-white" />
-          </div>
-          <h1 class="text-darken mb-3 pt-3 text-xl font-medium lg:h-14 lg:px-14">
-            GARDEN BED MAINTENANCE
-          </h1>
-          <p class="px-4 text-gray-500">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo iure inventore amet modi
-            accusantium vero perspiciatis, incidunt dicta sed aspernatur!
-          </p>
-        </div>
-      </div>
-    </div>
+  <div class="grid gap-8 md:grid-cols-3 md:gap-5">
+    <CardTuto categorie="php" />
+    <CardTuto categorie="js" />
+    <CardTuto categorie="css" />
   </div>
 </template>
 
