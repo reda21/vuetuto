df2e98d3bc2041f10f8903bbe3261ce0524e1c39
Reda Cherfaoui <redacherfaoui@gmail.com>
2025-04-16 14:24:20 +0100

    update Navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -74,6 +74,8 @@ const navLinks = [
   { name: 'Manga', path: '/manga' },
   { name: 'tailwind', path: '/tailwind' },
   { name: 'formulaire', path: '/formulaire' },
+  { name: 'button', path: '/design/button' },
+  { name: 'form', path: '/design/form' }
 ];
 
 const classBtn = 'transition-transform duration-300 ease-in-out transform hover:scale-110';
