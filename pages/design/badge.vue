2fa5493a0d44aa980837da9b4979477fd8716ab7
reda cherfaoui <redacherfaoui@gmail.com>
2025-04-25 00:10:05 +0100

    feat(badge): add new badge component and integrate with button
    This commit introduces a new badge component (`VBadge`) and integrates it with the existing button component. The badge component supports various severity levels and variants, and its styles are defined in a new CSS file (`badge.css`). Additionally, the button component has been refactored to use the new badge component instead of the previous inline badge implementation. The theme and variant CSS files have been updated to support the new badge styles. This change improves code maintainability and provides a more consistent UI experience.

---
+++ pages/design/badge.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1 class="mb-4 text-gray-900">VBadge</h1>
+    <div class="card flex flex-wrap justify-center gap-2">
+      <VBadge value="2"/>
+      <VBadge value="6" severity="secondary"/>
+      <VBadge value="8" severity="success"/>
+      <VBadge value="4" severity="info"/>
+      <VBadge value="9" severity="warn"/>
+      <VBadge value="3" severity="danger"/>
+      <VBadge value="5" severity="contrast"/>
+    </div>
+
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
